7431f7acee73b2291800aea5c0a652298e6c4547
RubenDev7 <97059915+RubenDev7@users.noreply.github.com>
2022-03-10 15:18:54 +0100

    google añadido pero no funciona, deja elegir cuenta pero no hacer login con ella

--- app/src/main/java/com/example/firebase_testing/AuthActivity.java
+++ app/src/main/java/com/example/firebase_testing/AuthActivity.java
@@ -5,11 +5,14 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -25,20 +28,28 @@ import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
  public class AuthActivity extends AppCompatActivity {
     EditText inputEmail;
     EditText inputPassword;
     Button register;
     Button loginEmail;
-    Button loginGoogle;
+    Button botonLoginGoogle;
 
     //585709910502-sodpt1knckp54pe2oablbjrt4ot9n9oo.apps.googleusercontent.com
+    private GoogleSignInClient googleClient;
+    private final int GOOGLE_SIGN_IN_CODE = 100;
+    private FirebaseAuth mAuth;
+
+     private GoogleSignInClient mGoogleSignInClient;
 
 
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
@@ -47,41 +58,93 @@ import com.google.firebase.auth.FirebaseAuth;
         b.putString("message","Integracion de FireBase completada");
         analytics.logEvent("InitScreen",b);
         this.setTitle("Autenticación");
+
+        //Se comprueba en preferencias si hay email y provider para iniciar sesión si lo hubiera
+        SharedPreferences prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE);
+        String email_activo = prefs.getString("email",null);
+        String provider_activo = prefs.getString("provider",null);
+        if (email_activo!=null && provider_activo!=null) {
+            abrirHome(email_activo, homeActivity.ProviderType.valueOf(provider_activo));
+        }
+
+
+
+
+        //llamada a metodos y casteo de botones
         main();
-        
+
     }
 
      private void main() {
         controls();
         registrarUsuario();
-        loginUsuario();
-        loginConGoogle();
+        loginUsuarioConEmail();
+        loginUsuarioConGoogle();
 
      }
 
+     private void loginUsuarioConGoogle() {
+            botonLoginGoogle.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
+                    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestIdToken("585709910502-sodpt1knckp54pe2oablbjrt4ot9n9oo.apps.googleusercontent.com")
+                            .requestEmail()
+                            .build();
 
-     private void loginConGoogle() {
-        loginGoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                    mGoogleSignInClient = GoogleSignIn.getClient(getApplicationContext(), gso);
+                    mGoogleSignInClient.signOut();
+                    Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                    //googleClient.signOut();
+                    startActivityForResult(signInIntent, GOOGLE_SIGN_IN_CODE);
 
-
-
-            }
-        });
+                }
+            });
      }
+
+     @Override
+     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+         super.onActivityResult(requestCode, resultCode, data);
+
+         if (requestCode == GOOGLE_SIGN_IN_CODE) {
+             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+             try {
+                 // Google Sign In was successful, authenticate with Firebase
+                 GoogleSignInAccount account = task.getResult(ApiException.class);
+
+            AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+            FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                    abrirHome(account.getEmail(),homeActivity.ProviderType.GOOGLE);
+                    } else {
+                        showAlert("pollas de google");
+                    }
+                }
+            });
+
+             } catch (ApiException e) {
+                 // Google Sign In failed, update UI appropriately
+                 e.printStackTrace();
+             }
+         }
+     }
+
+
 
      private void controls() {
         inputEmail = findViewById(R.id.editTextEmail);
         inputPassword = findViewById(R.id.editTextPassword);
         register = findViewById(R.id.buttonRegister);
         loginEmail = findViewById(R.id.buttonLogin);
-        loginGoogle = findViewById(R.id.buttonGoogle);
+        botonLoginGoogle = findViewById(R.id.buttonGoogle);
 
      }
 
-     private void loginUsuario() {
+     private void loginUsuarioConEmail() {
          loginEmail.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
@@ -108,8 +171,7 @@ import com.google.firebase.auth.FirebaseAuth;
          });
      }
 
-
-
+     
      private void registrarUsuario() {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/firebase_testing/homeActivity.java
+++ app/src/main/java/com/example/firebase_testing/homeActivity.java
@@ -2,6 +2,8 @@ package com.example.firebase_testing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,11 +23,21 @@ public class homeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         main();
         Bundle extras = getIntent().getExtras();
+
         String e = (String) extras.get("email");
         String p = (String) extras.get("provider");
         //String p = (String) extras.get("pass");
         email.setText(e);
         pass.setText(p);
+
+        //Guardado de datos
+
+        SharedPreferences prefs = (SharedPreferences) getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("email", e);
+        editor.putString("provider",p);
+        editor.commit();
+
     }
 
     private void main() {
@@ -44,13 +56,18 @@ public class homeActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
+                SharedPreferences prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.clear();
+                editor.commit();
                 onBackPressed();
             }
         });
     }
 
     public enum ProviderType {
-        BASIC
+        BASIC,
+        GOOGLE,
     }
 
 }
