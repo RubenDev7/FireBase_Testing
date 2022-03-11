22235b4b982c1fbec278ddc76c511607faac4cf3
RubenDev7 <97059915+RubenDev7@users.noreply.github.com>
2022-03-11 15:47:15 +0100

    no funciona el guardado de datos, pendiente revisar

--- app/src/main/java/com/example/firebase_testing/homeActivity.java
+++ app/src/main/java/com/example/firebase_testing/homeActivity.java
@@ -4,18 +4,70 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.drm.DrmStore;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentChange.Type;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldPath;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.MetadataChanges;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.Query.Direction;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.ServerTimestamp;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.firestore.Source;
+import com.google.firebase.firestore.Transaction;
+import com.google.firebase.firestore.WriteBatch;
+
+
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.HashMap;
 
 public class homeActivity extends AppCompatActivity {
     TextView email;
     TextView pass;
+    EditText direccion;
+    EditText telefono;
+
+    Button guardar;
+    Button recuperar;
+    Button borrar;
     Button cerrar;
+    final  FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+    String e = "";
+    String p = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +76,8 @@ public class homeActivity extends AppCompatActivity {
         main();
         Bundle extras = getIntent().getExtras();
 
-        String e = (String) extras.get("email");
-        String p = (String) extras.get("provider");
+        e = (String) extras.get("email");
+        p = (String) extras.get("provider");
         //String p = (String) extras.get("pass");
         email.setText(e);
         pass.setText(p);
@@ -42,14 +94,68 @@ public class homeActivity extends AppCompatActivity {
 
     private void main() {
         controls();
+        guardarDatos();
+        recuperarDatos();
+        borrarDatos();
         closeIntent();
     }
 
     private void controls() {
         email = findViewById(R.id.textViewEmail);
         pass = findViewById(R.id.textViewPassword);
+        guardar = findViewById(R.id.buttonGuardar);
+        recuperar = findViewById(R.id.buttonRecuperar);
+        borrar = findViewById(R.id.buttonBorrar);
         cerrar = findViewById(R.id.buttonClose);
+        direccion = findViewById(R.id.inputDireccion);
+        telefono = findViewById(R.id.inputTelefono);
     }
+
+    private void guardarDatos() {
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HashMap <String, Object> datosUsuario = new HashMap<>();
+                datosUsuario.put("provider",p);
+                datosUsuario.put("address",direccion.getText().toString().trim());
+                datosUsuario.put("phone",telefono.getText().toString().trim());
+
+                db.collection("users").document(e).set(datosUsuario).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),"Datos guardados", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(),"Vergazo", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void recuperarDatos() {
+        recuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void borrarDatos() {
+        borrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
+
+
 
     private void closeIntent() {
         cerrar.setOnClickListener(new View.OnClickListener() {
